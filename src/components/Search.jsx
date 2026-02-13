5ea864bc3cecb40e47f4739f2f41b538a07d9922
byIbrahimAli <59145840+byIbrahimAli@users.noreply.github.com>
2026-02-13 14:42:09 +0000

    Refactor CSS for consistency and readability; update Search component structure for improved layout

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -3,13 +3,15 @@ import React from 'react'
 const Search = ({ searchTerm, setSearchTerm }) => {
   return (
     <div className="search">
-      <img src="/search.svg" alt="search" />
-      <input
-        type="text"
-        placeholder="Search before food gets cold"
-        value={searchTerm}
-        onChange={(e) => setSearchTerm(e.target.value)}
-      />
+      <div>
+        <img src="/search.svg" alt="search" />
+        <input
+          type="text"
+          placeholder="Search before food gets cold"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+      </div>
     </div>
   )
 }
