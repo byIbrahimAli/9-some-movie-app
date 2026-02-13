d53ae92c5e67ffe0ffeb0de51740e10d82bd431a
byIbrahimAli <59145840+byIbrahimAli@users.noreply.github.com>
2026-02-13 13:26:55 +0000

    Refactor App component to enhance layout and content structure, introducing a header with an image and updated text for improved user engagement.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
 const App = () => {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-900 text-white">
-      <h1>sheeeeeese</h1>
-    </div>
+    <main>
+      <div className="pattern"/>
+
+      <div className="wrapper">
+        <header>
+          <img src="/hero.png" alt="hero banner" />
+          <h1>dry your eyes and your soul with <span className="text-gradient">endless entertainment</span></h1>
+          <p>search...</p>
+        </header>
+      </div>
+    </main>
   )
 }
 
